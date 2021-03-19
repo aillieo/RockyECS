b203aaaea7ec2b370a7d00e9b0c9bb3729c082e3
aillieo <aillieo@163.com>
2021-03-20 00:23:22 +0800

    update

--- Assets/Sample/Scripts/ECS/Components/C_MonsterGenerator.cs
+++ Assets/Sample/Scripts/ECS/Components/C_MonsterGenerator.cs
@@ -9,6 +9,38 @@ namespace Sample
     {
         public void Reset()
         {
+            ResetCounter();
+            current = null;
+            currentWave = 0;
+            timeForNextWave = 0;
+            timer = 0;
+            counter = 0;
+            monsterSequences.Clear();
+        }
+
+        public int currentWave;
+
+        public float timeForNextWave;
+
+        public float timer;
+        public int counter;
+
+        public List<LevelEntry.Wave> monsterSequences;
+        public LevelEntry.Wave current;
+
+        public void InitMonsters(List<LevelEntry.Wave> monsterSequences)
+        {
+            this.monsterSequences = monsterSequences;
+            currentWave = 0;
+            current = monsterSequences[currentWave];
+        }
+
+
+        public void ResetCounter()
+        {
+            timeForNextWave = current.delay;
+            timer = 0;
+            counter = 0;
         }
     }
 }
